ee630389d84f38efefa5a04b2201f990c02e95fc
Nick Semenkovich <semenko@alum.mit.edu>
2019-01-29 17:07:48 +0000

    Prep options page. Add storage manifest

---
+++ src/options/options.js
@@ -0,0 +1,48 @@
+// Derived from https://github.com/beaufortfrancois/extensions-update-notifier-chrome-extension
+
+var alertNetwork = document.querySelector('#alertNetwork');
+var alertWANIP = document.querySelector('#alertWANIP');
+var alertNameserver = document.querySelector('#alertNameserver');
+var alertUptime = document.querySelector('#alertUptime');
+var alertFirmware = document.querySelector('#alertFirmware');
+var alertLink = document.querySelector('#alertLink');
+
+// Saves synced options.
+function saveOptions() {
+  console.log('saving');
+  chrome.storage.sync.set({
+    alertNetwork: alertNetwork.checked,
+    alertWANIP: alertWANIP.checked,
+    alertNameserver: alertNameserver.checked,
+    alertUptime: alertUptime.checked,
+    alertFirmware: alertFirmware.checked,
+    alertLink: alertLink.checked,
+  });
+}
+
+// Restores synced preferences.
+window.onload = function() {
+  var ourKeys = {
+    alertNetwork: true,
+    alertWANIP: true,
+    alertNameserver: true,
+    alertUptime: true,
+    alertFirmware: true,
+    alertLink: true
+  };
+  chrome.storage.sync.get(ourKeys, function(results) {
+    console.log(results.alertNetwork);
+    alertNetwork.checked = results.alertNetwork;
+    alertWANIP.checked = results.alertWANIP;
+    alertNameserver.checked = results.alertNameserver;
+    alertUptime.checked = results.alertUptime;
+    alertFirmware.checked = results.alertFirmware;
+    alertLink.checked = results.alertLink;
+  });
+  alertNetwork.onchange = saveOptions;
+  alertWANIP.onchange = saveOptions;
+  alertNameserver.onchange = saveOptions;
+  alertUptime.onchange = saveOptions;
+  alertFirmware.onchange = saveOptions;
+  alertLink.onchange = saveOptions;
+}
